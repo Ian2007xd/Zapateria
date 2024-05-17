c8b53a4c62ad46a2b300bec2fdea285130018d4c
Ian2007xd <164522997+Ian2007xd@users.noreply.github.com>
2024-05-16 21:49:31 -0600

    Add files via upload

--- js/Principal.js
+++ js/Principal.js
@@ -1,13 +1,44 @@
-const nav=document.querySelector(".nav");
-window.addEventListener("scroll", function(){
-    nav.classList.toggle("active",window.scrollY > 0);
+const body = document.querySelector("body"), 
+        nav = document.querySelector("nav"), 
+        modeToggle = document.querySelector(".darlight"), 
+        searchToggle = document.querySelector(".searchToggle"),
+        sliderclose = document.querySelector(".sliderclose"),
+        slideropen = document.querySelector(".slideropen");
+
+        let getMode = localStorage.getItem("mode");
+        if(getMode && getMode === "dark-mode"){
+            body,classList.add("dark");
+        }
+
+modeToggle.addEventListener("click" , () => {
+    modeToggle.classList.toggle("activeLight");
+    body.classList.toggle("dark");
+    if (!bpdy.classList.contains("dark")){
+        localStorage.setItem("mode", "light-mode");
+    }else{
+        localStorage.setItem("mode", "dark-mode");
+    }
+});
+searchToggle.addEventListener("click" , () => {
+    searchToggle.classList.toggle("activeBar");
+});
+
+slideropen.addEventListener("click" , () => {
+    nav.classList.add("active");
+});
+
+body.addEventListener("click" , e => {
+    let clickedElm = e.target;
+    if (!clickedElm.classList.contains("slideropen") && !clickedElm.classList.contains("menu")){
+        nav.classList.remove("active");
+    }
 });
 
 let menuToggle = document.querySelector(".menu-toggle");
 let menuRedes = document.querySelector(".menu-redes");
 menuToggle.onclick = function(){
     menuRedes.classList.toggle("activeColor");
-}
+};
 
 /*
 const coords = {x: 0, y: 10};
